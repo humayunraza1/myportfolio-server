b0da7d2fa7970a19af511da98075068b24d949b8
Humayun Raza <humayunraza5@gmail.com>
2025-04-16 21:33:12 +0500

    allowed only vercel and localhost to access server ~H

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors( {
+  origin: 'https://humayunraza.vercel.app/'
+}));
 app.use(express.json());
 
 // Contact form endpoint
